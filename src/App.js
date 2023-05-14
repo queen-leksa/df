4fef4a17307efc8b9d307876d735a3de438cbf1a
Leksa <lexysnake@gmail.com>
2023-05-14 18:27:42 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+
+const Main = () => {
+  return <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+}
 
 function App() {
   return (
+      <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/111">Link</Link>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/123" element={<h1>123</h1>}/>
+        <Route path="/111" element={<h1>111</h1>}/>
+      </Routes>
     </div>
+      </BrowserRouter>
   );
 }
 
